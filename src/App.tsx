8acac48aae5b6acf6c704ada3ccb1574ce267ea8
Joanna Chen <joannachenchencom@gmail.com>
2024-07-24 12:10:34 +0800

    refactor: app & demo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,34 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Box, Button, Card, Container, Divider, FormControl, Grid, InputLabel, List, ListItem, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material'
-import { Outlet, Link } from 'react-router-dom'
+import { Box, Button, Card, CardContent, Container, Divider, Grid, TextField, Typography } from '@mui/material'
+import { Outlet, Link, useNavigate } from 'react-router-dom'
 import { HubConnection } from '@microsoft/signalr'
 import { createConnection } from './signalRConnection'
-import dayjs from 'dayjs'
-import ChatList from './components/chatList'
-import { BaseObject, BroadCastType, GroupMessageType, MessageType, PrivateMessageType } from './types'
+import { BaseObject } from './types'
 
+import CatIcon from '/cat.svg'; 
+import DogIcon from '/dog.svg'; 
 
 function App() {
   const groupList = [
     {
       id: "cat",
       name: "貓派",
+      icon: CatIcon
     },
     {
       id: "dog",
       name: "狗派",
+      icon: DogIcon
     },
   ];
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [connection, setConnection] = useState<HubConnection|null>(null);
   const [connectionStatus, setConnectionStatus] = useState(false);
 
   const [currentUser, setCurrentUser] = useState('');
-  const [currentGroup, setCurrentGroup] = useState<BaseObject>();
-
-  // 訊息輸入：
-  const [globalMessage, setGlobalMessage] = useState('');
-  const [groupMessage, setGroupMessage] = useState('');
-  const [privateMessage, setPrivateMessage] = useState('');
-
-  // 紀錄訊息列表：
-  const [globalMessages, setGlobalMessages] = useState<MessageType[]>([]);
-  const [groupMessages, setGroupMessages] = useState<GroupMessageType[]>([]);
-  const [privateMessages, setPrivateMessages] = useState<PrivateMessageType[]>([]);
+  const [currentGroups, setCurrentGroups] = useState<BaseObject[]>([]);
 
 
   const handleConnection = async() => {
@@ -56,264 +49,64 @@ function App() {
     }
   }
 
-  const handleBasicMessage = (content: string): MessageType => {
-    const message: MessageType = {
-      user: currentUser,
-      traceId: `${new Date().getTime()}`,
-      content,
-      createdTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-    };
-
-    return message;
-  }
-
-  const handleJoinGroup = async (groupId: string) => {
-    if(!connectionStatus || !connection){
-      alert("尚未進入聊天室！");
-      return;
-    }
-
-    try{
-      await connection.invoke("JoinGroup", groupId);
-      alert("JoinGroup: " + groupId);
-    }catch(error){
-      console.log("handleJoinGroup error: ", error);
-      alert("Join Group failurely!");
-    }
-  }
-
-  const handleLeaveGroup = async (groupId: string) => {
-    if(!connectionStatus || !connection){
-      alert("尚未進入聊天室！");
-      return;
-    }
-
-    try{
-      await connection.invoke("LeaveGroup", groupId);
-      alert("LeaveGroup: " + groupId);
-    }catch(error){
-      console.log("handleLeaveGroup error: ", error);
-      alert("Leave Group failurely!");
-    }
-  }
-
-  const handleSendGroupMessage = async () => {
-    if(!connectionStatus || !connection){
-      alert("尚未進入聊天室！");
-      return;
-    }
-    if(!currentGroup){
-      alert("尚未選擇群組！");
-      return;
-    }
-
-    try{
-      const basicMessage = handleBasicMessage(groupMessage);
-      const message: GroupMessageType = {
-        ...basicMessage,
-        groupName: currentGroup.name,
-        groupId: currentGroup.id,
-      };
-      await connection.invoke('SendGroupMessage', currentGroup.id, message);
-
-      setGroupMessage('');
-      setCurrentGroup(undefined);
-    }catch(error){
-      console.log("handleSendGroupMessage error: ", error);
-      alert("Send Group Message failurely!");
-    }
-  }
-
-  const handleSendGlobalMessage = async () => {
-    if(!connectionStatus || !connection){
-      alert("尚未進入聊天室！");
-      return;
-    }
-    
-    try{
-      // Send Message To Server
-      const message: MessageType = handleBasicMessage(globalMessage);
-      await connection.invoke('SendGlobalMessage', message);
-
-      setGlobalMessage('');
-    }catch(error){
-      console.log("handleSendGlobalMessage error: ", error);
-      alert("Send Global Message failurely!");
-    }
-  }
-
-  useEffect(() => {
-    if(!connectionStatus || !connection){
-      return;
-    }
-
-    const handleGlobalMessage = (data: string) => {
-      const record = JSON.parse(data);
-      setGlobalMessages(prev => [...prev, { ...record }]);
-    };
-
-    const handleGroupMessage = (data: string) => {
-      console.log("🚀 ~ handleGroupMessage ~ data:", data)
-      const record = JSON.parse(data);
-      setGroupMessages(prev => [...prev, { ...record }]);
-    };
-
-    const handlePrivateMessage = (data: string) => {
-      const record = JSON.parse(data);
-      setPrivateMessages(prev => [...prev, { ...record }]);
-    };
-
-    // 建立監聽：GlobalMessage
-    connection.on('GlobalMessage', handleGlobalMessage)
-
-    // 建立監聽：GroupMessage
-    connection.on('GroupMessage', handleGroupMessage)
-
-    // 建立監聽：1on1 Message
-    connection.on('PrivateMessage', handlePrivateMessage)
-
-    return () => {
-      // 停止監聽
-      connection.off('GlobalMessage', handleGlobalMessage)
-      connection.off('GroupMessage', handleGroupMessage)
-      connection.off('PrivateMessage', handlePrivateMessage)
-      // 關閉連線
-      connection.stop();
-    }
-  }, [connectionStatus])
-
   return (
     <>
-      <h1>WebSocket Demo</h1>
-      <section className="card">
-        {/* <h2>系列一：推播範圍</h2> */}
-        {/* <Link to={'/broadcast'}>Broad</Link> */}
-
+      <Container>
+        <Typography variant='h2' gutterBottom>WebSocket Demo</Typography>
         <TextField
+          fullWidth
           variant="outlined"
+          size="small"
           label="Name"
           value={currentUser}
           onChange={(e) => setCurrentUser(e.target.value)}
-          
         />
-        <Button variant="contained" color="primary" onClick={handleConnection} >
-          加入
-        </Button>
-        <h3>目前連線狀態：{connectionStatus ? "連線中" : "尚未連線"}</h3>
-        <Container>
-        <Grid container spacing={10} p={2}>
+        
+        <Grid container spacing={2} my={2}>
           {groupList.map(item => (
             <Grid item xs={12} md={6} key={item.id}>
-              <Card variant="elevation" style={{margin: 10, padding: 10}}>
-                <h4>{item.name}</h4>
+              <Card elevation={3}>
+                <CardContent>
+                 
+                  <Box><img src={item.icon} alt={item.name} /></Box>
+                  <Typography variant='h4' gutterBottom>{item.name}</Typography>
+             
+                  {
+                      currentGroups.some(group => group.id === item.id)
+                      ? (
+                          <Button 
+                            variant="contained"
+                            disabled
+                          >
+                            已加入
+                          </Button>
+                      ) : (
+                          <Button 
+                            onClick={() => setCurrentGroups([...currentGroups, item])} 
+                            variant="outlined"
+                            color="success"
+                          >
+                            加入群組
+                          </Button>
+                      )
+                    }
+                </CardContent>
 
-                <Button 
-                  onClick={() => handleJoinGroup(item.id)} 
-                  variant="contained"
-                  color="success"
-                >
-                  加入群組
-                </Button>
-
-                <Button 
-                  onClick={() => handleLeaveGroup(item.id)} 
-                  variant="contained"
-                  color="warning"
-                >
-                  離開群組
-                </Button>
               </Card>
             </Grid>
           ))}
         </Grid>
-        </Container>
-      </section>
-      <Divider />
 
-      <Container>
-        <Grid container spacing={10} p={2}>
-          <Grid item xs={12} md={6}>
-            <Box mb={5} p={1}>
-              {/* Global Broadcast */}
-              <Typography variant="h4" gutterBottom>
-                Global Broadcast
-              </Typography>
-  
-              <TextField
-                variant="outlined"
-                label="Message"
-                value={globalMessage}
-                onChange={(e) => setGlobalMessage(e.target.value)}
-                fullWidth
-              />
-              <Button onClick={handleSendGlobalMessage} variant="contained" color="primary">
-                發送訊息
-              </Button>
-              
-            </Box>
-          </Grid>
-
-          {/* Messages Display */}
-          <Grid item xs={12} md={6}>
-            <ChatList
-              type={BroadCastType.GLOBAL}
-              messages={globalMessages}
-            />
-          </Grid>
-        </Grid>
+        <Button 
+          fullWidth
+          variant="contained" 
+          color="inherit" 
+          onClick={() => navigate('/demo')}
+        >
+          進入聊天室
+        </Button>
       </Container>
-
-      <Divider />
-
-      <Container>
-        <Grid container spacing={10} p={2}>
-          <Grid item xs={12} md={6}>
-          <Box mb={5}>
-              {/* Group Broadcast */}
-              <Typography variant="h4">Group Broadcast</Typography>
-              <FormControl fullWidth>
-                  <InputLabel>Group</InputLabel>
-                  <Select
-                      value={currentGroup}
-                      onChange={(e: SelectChangeEvent<BaseObject>) => setCurrentGroup({
-                        name: e.target.name,
-                        id: e.target.value.toString()
-                      })}
-                  >
-                    {groupList.map(item => (
-                      <MenuItem value={item.id}>{item.name}</MenuItem>
-                    ))}
-                  </Select>
-              </FormControl>
-              <TextField
-                  variant="outlined"
-                  label="Message"
-                  value={groupMessage}
-                  onChange={(e) => setGroupMessage(e.target.value)}
-                  fullWidth
-              />
-              <Button 
-                onClick={handleSendGroupMessage} 
-                variant="contained"
-                color="primary"
-              >
-                發送訊息
-              </Button>
-
-            </Box>
-          </Grid>
-
-          {/* Messages Display */}
-          <Grid item xs={12} md={6}>
-            <ChatList
-              type={BroadCastType.GROUP}
-              messages={groupMessages}
-            />
-          </Grid>
-          
-        </Grid>
-      </Container>
-
+      
       <main>
         <Outlet />
       </main>
